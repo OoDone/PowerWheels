ded5f1e76dcd2516bcca6fb2ca29a197adc64ea3
OoDone <48130949+OoDone@users.noreply.github.com>
2022-04-18 14:32:51 -0400

    Update DriveMotor.py

--- drive/DriveMotor.py
+++ drive/DriveMotor.py
@@ -3,8 +3,13 @@ sys.path.append('/home/Desktop/PowerWheels/')
 from ..Constants import Constants 
 speed = 0.0
 constants = Constants()
+motor = False
 
 class DriveMotor:
+  
+  def __init__(motorPin):
+    motor = motorPin
+    
   def driveMotor(speedPercent):
     speed = 0.0
     if speedPercent > 0:
@@ -12,7 +17,7 @@ class DriveMotor:
     else:
       speed = speedPercent*5+motorNeutralSpeed
     driveSpeed = speed
-    pi.set_servo_pulsewidth(ESC, speed)
+    pi.set_servo_pulsewidth(motor, speed)
 
 #Getters for encoder ticks, motor speed
 #Encoder logic
