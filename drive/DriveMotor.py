1e561ad27830de4642433fe7035972786181d015
OoDone <48130949+OoDone@users.noreply.github.com>
2022-04-18 13:18:22 -0400

    Update DriveMotor.py

--- drive/DriveMotor.py
+++ drive/DriveMotor.py
@@ -1,1 +1,13 @@
+import Constants
+speed = 0.0
+constants = Constants()
 
+class DriveMotor:
+  def driveMotor(speedPercent):
+    speed = 0.0
+    if speedPercent > 0:
+      speed = motorNeutralSpeed+speedPercent*5
+    else:
+      speed = speedPercent*5+motorNeutralSpeed
+    driveSpeed = speed
+    pi.set_servo_pulsewidth(ESC, speed)
