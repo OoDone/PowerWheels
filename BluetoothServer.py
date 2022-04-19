c0002a154f65520aa677d385fafc9ac656b8c4ac
OoDone <48130949+OoDone@users.noreply.github.com>
2022-04-19 10:19:53 -0400

    Update BluetoothServer.py

--- BluetoothServer.py
+++ BluetoothServer.py
@@ -2,6 +2,7 @@ from Logger import Logger
 logger = Logger("robotLog")
 
 class BluetoothServer:
+  connected = False
   bd_addr = "DC:A6:32:6B:38:BD"  #"B8:27:EB:D6:57:CE" 
   #B8:27:EB:6B:AB:4B
   uuid = "42b58f76-b26d-11ea-b733-cb205305bc99"
@@ -10,19 +11,23 @@ class BluetoothServer:
   def __init__():
     logger.info("Robot | Code: BluetoothServer.py Init.")
     
+    
   server_socket = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
   server_socket.bind((bd_addr, bluetooth.PORT_ANY))
   server_socket.listen(port)
   logger.info("Bluetooth Bind: Listening on port " + str(port))
   
+  #enabledAlert(0.1, 3)
   bluetooth.advertise_service(server_socket, "SampleServer", service_classes=[bluetooth.SERIAL_PORT_CLASS],profiles=[bluetooth.SERIAL_PORT_PROFILE])
   logger.info("Bluetooth: Advertising Service!")
 
 
   client_socket, address = server_socket.accept()
   logger.info("Bluetooth: Accepting client!")
-  #server_socket.send('\x1a')
   logger.info("Bluetooth: Device connected!")
+  client_socket.send("connected") #ADD CODE TO HANDLE ON CLIENT SIDE
+  connected = True
+  #enabledAlert(0.2, 2) #2 bluetooth connected 
     
   def return_data():
     try:
@@ -34,14 +39,15 @@ class BluetoothServer:
             return data
     except OSError:
         pass
+   
       
   while True:
     x=return_data()
     if x == None:
       logger.info("Bluetooth: disconnected!")
       pi.set_servo_pulsewidth(ESC, 0)
-      disconnected = True
+      connected = False
       client_socket, address = server_socket.accept()
-      if disconnected == True:
+      if connected == False:
         logger.info("Bluetooth: Reconnected!")
   
