f1c058e2162673f91f2d48127fc2592fa96fe58b
OoDone <48130949+OoDone@users.noreply.github.com>
2022-04-27 11:01:57 -0400

    Update Servo.py

--- drive/Servo.py
+++ drive/Servo.py
@@ -13,7 +13,7 @@ position = 0.0
 servo = False
 
 class Servo:
-  def __init__(self, motorPin, Logger):
+  def __init__(self, servoPin, Logger):
     global servo
     global logger
     global constants
@@ -21,7 +21,7 @@ class Servo:
     logger = Logger
     logger.info("Robot | Code: Motor.py Init.")
     constants = Constants()
-    servo = constants.DriveConstants().servoPin
+    servo = servoPin
     GPIO.setmode(GPIO.BCM)
     if constants.isTestingMode == False:
       os.system("sudo pigpiod")
@@ -40,21 +40,21 @@ class Servo:
     if positionPercent > -101 and positionPercent < 101:
       position = 0.0
       if positionPercent > 0:
-        position = constants.DriveConstants().motorNeutralSpeed+positionPercent*5
+        position = constants.DriveConstants().servoNeutralPosition+positionPercent*5
       else:
-        position = positionPercent*5+constants.DriveConstants().motorNeutralSpeed
+        position = positionPercent*5+constants.DriveConstants().servoNeutralPosition
       if constants.isTestingMode == False:
         pi.set_servo_pulsewidth(servo, position)
       else: logger.info("TestMode: Set Servo Position to " + str(positionPercent))
     else: logger.info("setServoPositionPercent: positionPercent not within allowed position range.")
     
-  def stopMotor(self):
+  def stopServo(self):
     if constants.isTestingMode == False:
-      pi.set_servo_pulsewidth(motor, 0.0)
-    else: logger.info("TestMode: Stopping motor...")
+      pi.set_servo_pulsewidth(servo, 0.0)
+    else: logger.info("TestMode: Stopping Servo...")
     
-  def getDriveSpeedPercent(self):
-    return speed
+  def getServoPosition(self):
+    return position
 
 #Getters for encoder ticks, motor speed
 #Encoder logic
