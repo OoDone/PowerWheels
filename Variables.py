aed86e0723bc43ac65c0982f70d9a8cb8ab51a83
JE5143 <discord1qq@gmail.com>
2022-04-29 17:00:10 -0400

    servo max and min limit variable

--- Variables.py
+++ Variables.py
@@ -1,7 +1,7 @@
 #Example use:     constants.DriveConstants().ESC
 class Constants:
 
-  isTestingMode = False#Bypasses Need to be ran on Pi, disables bluetooth server, ETC
+  isTestingMode = True #Bypasses Need to be ran on Pi, disables bluetooth server, ETC
 
   #robot constants
   ultrasonicSensorEnabled = False
@@ -16,8 +16,8 @@ class Constants:
     ESC = 4
     servoNeutralPosition = 1700 #1488 for 556-2420 & 1700 for 1500-1900
     directionTicksPer = 2 #(Ticks of rotation)/100 #100 is for input value
-    servoMaxLimitTicks = 1 #FIXME 
-    servoMinLimitTicks = 1 #FIXME
+    servoMaxLimitTicks = 1900 #FIXME 
+    servoMinLimitTicks = 1500 #FIXME
     distanceTicks = 1000
 
   #Auton Constants
@@ -27,7 +27,7 @@ class Constants:
   
   #Bluetooth Constants
   class BluetoothConstants:
-    bd_addr = "DC:A6:32:6B:38:BD"#"DC:A6:32:6B:38:BD"  #"B8:27:EB:D6:57:CE" 
+    bd_addr = ""#"DC:A6:32:6B:38:BD"  #"B8:27:EB:D6:57:CE" 
     #B8:27:EB:6B:AB:4B
     uuid = "42b58f76-b26d-11ea-b733-cb205305bc99"
     port = 1
