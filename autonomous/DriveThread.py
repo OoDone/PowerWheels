6dab4818b48b2d0ef1ba11e79ac2632562627d0d
OoDone <discord1qq@gmail.com>
2022-05-26 15:16:19 +0000

    Add DriveThread file, and class, and DriveAuton class

---
+++ autonomous/DriveThread.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+import os
+import threading
+
+
+
+start = False
+stop = False
+driveRobot = False
+driveDirection = False #False is forwards
+class AutonDrive:
+    def __init__(self, Logger):
+        global logger
+        logger = Logger
+        logger.info("Robot | Code: DriveAuton Class Init")
+    
+    def stopRobot():
+        global stop
+        stop = True
+        logger.info("DriveThread: Stopping robot..")
+
+    def driveRobot(isReverse):
+        #if isReverse is true, drive backwards, else drive forwards
+        global driveRobot
+        global driveDirection
+        driveRobot = True
+        driveDirection = isReverse
+        logger.info("DriveThread: Driving Robot...")
+
+
+class DriveThread(threading.Thread):
+    def __init__(self, Logger, threadID, Start):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = "Drive-" + str(threadID)
+        global logger
+        global start
+        start = Start
+        logger = Logger
+        logger.info("Robot | Code: DriveThread.py Init")
+        
+
+        
+ 
+   
+
+
+
+    def run(self):
+        logger.info("Starting Drive Thread: " + self.name)
+        global start
+        while start:
+            logger.info("Run Loop")
+        else:
+            logger.info("Stopping Drive Thread")
+            return #Stop Drive Thread
+
+    def stopDrive(self):
+        global start
+        start = False
+            
+        
