7aff2b463858d15ba02f09a2936f5a959e92f81a
OoDone <48130949+OoDone@users.noreply.github.com>
2022-09-09 10:44:00 -0400

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -54,7 +54,9 @@ class Constants:
 
   #Bluetooth Constants
   class BluetoothConstants:
-    bd_addr = ""#"DC:A6:32:6B:38:BD"  #"B8:27:EB:D6:57:CE" 
+    bd_addr = "" #Leave blank to accept connections from any device, enter a address to only allow a specific device
+    #Example bd_addr = "DC:A6:32:6B:38:BD"  
+    #"B8:27:EB:D6:57:CE" 
     #B8:27:EB:6B:AB:4B
     uuid = "42b58f76-b26d-11ea-b733-cb205305bc99"
     port = 1
